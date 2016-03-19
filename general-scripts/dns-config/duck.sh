57c1863b0170db3a5b01eac9df605a889086f7a7
Jordan Thayer <jordan.thayer@gmail.com>
2016-03-19 10:03:08 -0400

    Refactor
    Renamed the TOKEN variable to DUCKAUTHTOKEN.  Several things will likely need
    tokens, I don't want to clobber things that are already in the environment, and
    this is a pretty specific name for what we're doing.

--- general-scripts/dns-config/duck.sh
+++ general-scripts/dns-config/duck.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 DOMAIN="domainname"
-TOKEN="WILLIAMS"
 LOGS="BIGHEAVYWOOD"
+DUCKAUTHTOKEN="WILLIAMS"
 
-echo url="https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&ip=" | curl -k -o $LOGS -K -
+echo url="https://www.duckdns.org/update?domains=$DOMAIN&token=$DUCKAUTHTOKEN&ip=" | curl -k -o $LOGS -K -
 
 ## Still needs to be added to a cron tab, also run once at startup!
