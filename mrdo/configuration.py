03394d183dec9440f07bd7f1888dbb8bbbd57633
Jordan Thayer <jordan.thayer@gmail.com>
2016-03-29 16:59:32 -0400

    New Feature:  I/O for configuration files, some lite validation before saving, loading

--- mrdo/configuration.py
+++ mrdo/configuration.py
@@ -1,6 +1,6 @@
 import json
 
-def Configuration(object):
+class Configuration(object):
     DO_API_KEY = 'DO_API_KEY'
     IRC_PASSWD = 'IRC_PASSWORD'
     IRC_UNAME  = 'IRC_USER_NAME'
@@ -11,17 +11,60 @@ def Configuration(object):
     USER = 1
     BLESSED = 2
 
-    def __init__(settings):
+    def __init__(self,settings):
         """
         Create an object representing the configuration of MrDo
         """
         self.settings = settings
 
-    def from_json(json_obj):
+    @staticmethod
+    def from_path(path):
         """
         Create an object representing the configuration of MrDo from a json object
         """
-        raise Exception("stub: from_json")
+        f = open(path, 'r')
+        settingsDict = json.load(f)
+        f.close()
+        to_ret = Configuration(settingsDict)
+        if not to_ret._validate():
+            raise Exception("Error loading configuration from " + path + ": invalid settings.")
+        return to_ret
+
+    def to_path(self, path):
+        """
+        Convert the object to json and then stow it at path
+        Will clobber any existing configuration file living there
+        """
+        if self._validate():
+            f = open(path,'w')
+            json.dump(self.settings, f, sort_keys=True, indent=4)
+            f.close()
+        else:
+            raise Exception("Can't convert invalid configuration to JSON.")
+
+    def _validate(self):
+        """
+        Run a sanity check on the object to make sure it's worth saving
+        """
+        ret = True
+
+        if not self.settings.has_key(Configuration.DO_API_KEY):
+            print "No Digital Ocean API Key"
+            ret = False
+        if not self.settings.has_key(Configuration.IRC_PASSWD):
+            print "No IRC Password for Mr. Do"
+            ret = False
+        if not self.settings.has_key(Configuration.IRC_UNAME):
+            print "No User Name for Mr. Do"
+            ret = False
+        if not self.settings.has_key(Configuration.IRC_BLESSD):
+            print "No list of users for Mr. Do"
+            ret = False
+        if not self.settings.has_key(Configuration.IRC_OPS):
+            print "No operators for Mr. Do"
+            ret = False
+
+        return ret
 
     def _get_auth_level(self, user):
         """
@@ -64,39 +107,3 @@ def Configuration(object):
         Adds a digital ocean API key to the assoc list of users -> api keys
         """
         raise Exception("Stub: add_api_key")
-
-    def to_json(self):
-        """
-        Convert the object to json for storage / transmission
-        """
-        raise Exception("Stub: to_json")
-
-    def to_path(self, path):
-        """
-        Convert the object to json and then stow it at path
-        """
-        raise Exception("stub: to_path")
-
-    def _validate(self):
-        """
-        Run a sanity check on the object to make sure it's worth saving
-        """
-        ret = True
-
-        if not self.settings.has_key(DO_API_KEY):
-            print "No Digital Ocean API Key"
-            ret = False
-        if not self.settings.has_key(IRC_PASSWD):
-            print "No IRC Password for Mr. Do"
-            ret = False
-        if not self.settings.has_key(IRC_UNAME):
-            print "No User Name for Mr. Do"
-            ret = False
-        if not self.settings.has_key(IRC_BLESSD):
-            print "No list of users for Mr. Do"
-            ret = False
-        if not self.settings.has_key(IRC_OPS):
-            print "No operators for Mr. Do"
-            ret = False
-
-        return ret
