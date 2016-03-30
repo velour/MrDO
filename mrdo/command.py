7e6a0b9ac8dd8d545dca476bfb858d02a2eca4c2
Seabass <jordan.thayer@gmail.com>
2016-03-30 06:36:45 -0400

    New Stub: Stubbing out commands I'd like to support in Mr. Do

--- mrdo/command.py
+++ mrdo/command.py
@@ -1,4 +1,4 @@
-from configuration import Configurationo
+from configuration import Configuration
 
 class Command(object):
     def __init__(self, keyword, shortDesc, longDesc, auth=Configuration.OP):
@@ -12,3 +12,36 @@ class Command(object):
         Can the command be issued by the user in questin according to the config
         """
         return config.get_auth_level(issuer) >= self.auth
+
+    def __str__(self):
+        return self.keyword
+
+## Commands
+help = Command("help",
+               "Get help using the bot.",
+               """List all commands the bot can accept, or ask for
+               help on a specific command see detailed information
+               along with a usage string.""",
+               auth = Configuration.NO_ONE)
+
+# User Management
+op_user = None
+unop_user = None
+add_user = None
+rem_user = None
+
+# Droplet Manipulation
+add_api_key = None
+stop_droplet = None
+list_images = None
+load_most_recent_image = None
+load_named_image = None
+running_droplet = None
+
+# Game Server Management
+list_inits = None
+list_ongoing = None
+start_server = None
+backup_server = None
+stop_server = None
+
